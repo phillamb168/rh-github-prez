044ce9de5b7580aed72a66fe446310388b658f69
Phillip Lamb <Phillip.lamb@nc.gov>
2020-07-28 16:47:43 -0400

    trying something else

--- devops/prod/main.tf
+++ devops/prod/main.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket  = "rh-github-prez-tfstate"
-    key     = "${path_relative_to_include()}/terraform.tfstate"
+    key     = "./terraform.tfstate"
     region  = "us-east-1"
     encrypt = true
     profile = "rhgithubprez"
