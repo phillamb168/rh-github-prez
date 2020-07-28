18f0de89858130335cd23e8931626fc4f51c8934
Phillip Lamb <Phillip.lamb@nc.gov>
2020-07-28 16:38:52 -0400

    modifying main.tf to include vars

--- devops/prod/main.tf
+++ devops/prod/main.tf
@@ -1,3 +1,9 @@
 terraform {
-  backend "s3" {}
+  backend "s3" {
+    bucket = "rh-github-prez-tfstate"
+    key = "${path_relative_to_include()}/terraform.tfstate"
+    region = "us-east-1"
+    encrypt = true
+    profile = "rhgithubprez"
+  }
 }
